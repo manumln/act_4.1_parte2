cbfc61fb5655bd86a7fa56575ea5ad4c8801a171
encarni <emr@gmail.com>
2023-04-14 01:13:17 +0200

    Versión E.0.1 Documentar clase cociente.

--- calculadora/Cociente.java
+++ calculadora/Cociente.java
@@ -1,4 +1,18 @@
+/*División de dos números reales, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+División de dos números enteros, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+Inverso de un números real, tendrá un parámetros de entrada y uno de salida que será la solución.
+Raíz de un números, tendrá un parámetros de entrada y uno de salida que será la solución.
+ */
 package calculadora;
+
+/**
+ * Clase en la que se engloban todos los métodos de la división en la calculadora
+ * @see calculadora
+ * @author E.M.R
+ * @version 1.0.0
+ * @since 14/04/2023
+ *
+ */
 
 
 public class Cociente {
